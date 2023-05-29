b0af34f18e0845387326a949c5ae004e54fa5ca3
maxyuri99 <maxyurisantos@hotmail.com>
2023-05-28 21:22:36 -0300

    Primeiro Commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,39 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+
+function handleModal(){
+    const buttons = document.querySelectorAll('.subscribe__button')
+    const modalController = document.querySelector('.modal__controller')
+
+    for(let i = 0; i < buttons.length; i++){
+        const button = buttons[i]
+        button.addEventListener('click', function(){
+            modalController.showModal()
+
+            closeModal()
+            closeModalOutlineClick()
+        })
+    }
+}
+
+function closeModal(){
+    const button = document.querySelector('.modal__close')
+    const modalController = document.querySelector('.modal__controller')
+
+    button.addEventListener('click', function(){
+        modalController.close()
+    })
+}
+
+function closeModalOutlineClick(){
+    const body = document.querySelector('body')
+
+    body.addEventListener('click', function(event){
+        if(event.target.classList.contains('modal__controller')){
+            const modal = document.querySelector('.modal__controller')
+
+            modal.close()
+        }
+    })
+}
+
+handleModal()
