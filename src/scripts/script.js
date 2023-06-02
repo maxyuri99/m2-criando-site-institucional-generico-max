a2504616488aca2a05711a98a57f4a846518b628
maxyuri99 <maxyurisantos@hotmail.com>
2023-06-02 15:39:03 -0300

    Ultimo Commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,12 +1,12 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
 
 function handleModal(){
-    const buttons = document.querySelectorAll('.subscribe__button')
-    const modalController = document.querySelector('.modal__controller')
+    const buttons = document.querySelectorAll(".subscribe__button")
+    const modalController = document.querySelector(".modal__controller")
 
     for(let i = 0; i < buttons.length; i++){
         const button = buttons[i]
-        button.addEventListener('click', function(){
+        button.addEventListener("click", function(){
             modalController.showModal()
 
             closeModal()
@@ -16,20 +16,20 @@ function handleModal(){
 }
 
 function closeModal(){
-    const button = document.querySelector('.modal__close')
-    const modalController = document.querySelector('.modal__controller')
+    const button = document.querySelector(".modal__close")
+    const modalController = document.querySelector(".modal__controller")
 
-    button.addEventListener('click', function(){
+    button.addEventListener("click", function(){
         modalController.close()
     })
 }
 
 function closeModalOutlineClick(){
-    const body = document.querySelector('body')
+    const body = document.querySelector("body")
 
-    body.addEventListener('click', function(event){
-        if(event.target.classList.contains('modal__controller')){
-            const modal = document.querySelector('.modal__controller')
+    body.addEventListener("click", function(event){
+        if(event.target.classList.contains("modal__controller")){
+            const modal = document.querySelector(".modal__controller")
 
             modal.close()
         }
